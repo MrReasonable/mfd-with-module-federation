60fac88212ec8d8bd8afeee4d254f78fb2f7736d
Ian Dominey <ian.dominey@gmail.com>
2022-05-08 21:52:57 +0100

    Fixed webpack config issue for dashboard prod build.

--- packages/dashboard/config/webpack.prod.js
+++ packages/dashboard/config/webpack.prod.js
@@ -14,7 +14,7 @@ const prodConfig = {
       name: "dashboard",
       filename: "remoteEntry.js",
       exposes: {
-        "./DashboardApp": "./src/bootstrap.jsx",
+        "./DashboardApp": "./src/bootstrap.js",
       },
       shared: packageJson.dependencies,
     }),
